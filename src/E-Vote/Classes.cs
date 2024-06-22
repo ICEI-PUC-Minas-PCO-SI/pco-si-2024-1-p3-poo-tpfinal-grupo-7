0874a3c07210ebcd7cd41db5a4e4faaa0013960f
Vinicius Augusto <viniaugusto129@gmail.com>
2024-06-22 05:37:48 -0300

    modificando o nome da classe "PartidoJudiciario" para "PartidoLegislativo" e adicionando a classe "Legislativo"

--- src/E-Vote/Classes.cs
+++ src/E-Vote/Classes.cs
@@ -39,19 +39,19 @@ namespace Classes
         }
     }
 
-    public class PartidoJudiciario : Partido
+    public class PartidoLegislativo : Partido
     {
         private int quocienteEleitoral;
         private int cadeiras;
 
-        public PartidoJudiciario(string _nome) : base(_nome)
+        public PartidoLegislativo(string _nome) : base(_nome)
         {
 
         }
 
         public void calcularCadeiras(int votosRecebidos, double quocienteEleitoral)
         {
-            this.cadeiras = (int) (votosRecebidos / quocienteEleitoral);
+            this.cadeiras = (int)(votosRecebidos / quocienteEleitoral);
         }
 
         public int getCadeiras()
@@ -59,7 +59,7 @@ namespace Classes
             return this.cadeiras;
         }
 
-        public void calcularQuociente(int totalDeVotos,int cadeirasDisponiveis)
+        public void calcularQuociente(int totalDeVotos, int cadeirasDisponiveis)
         {
             this.quocienteEleitoral = totalDeVotos / cadeirasDisponiveis;
         }
@@ -70,98 +70,113 @@ namespace Classes
         }
     }
 
-    public class Usuario{ // Mudar pras do pedro depois q ele commitar
+    public class Usuario
+    { // Mudar pras do pedro depois q ele commitar
 
-       int cpf;
+        int cpf;
 
-       string nome;
+        string nome;
 
-       string senha;
+        string senha;
 
-       bool adm = false;
+        bool adm = false;
 
-       public int getCpf(){ 
-        return this.cpf;
-       }
-
-       public void setCpf(int _cpf){
-         this.cpf = _cpf;
-       }
-
-        public string getNome(){
-        return this.nome;
-       }
-
-       public void setNome(string _nome){
-         this.nome = _nome;
-       }
-
-        public string getSenha(){
-        return this.senha;
-       }
-
-       public void setSenha(string _senha){
-         this.senha = _senha;
-       }
-
-    
-
-       public Usuario(int cpf_, string senha_, string nome_)
+        public int getCpf()
         {
-            cpf=cpf_;
-            senha=senha_;
-            nome=nome_;
+            return this.cpf;
+        }
+
+        public void setCpf(int _cpf)
+        {
+            this.cpf = _cpf;
+        }
+
+        public string getNome()
+        {
+            return this.nome;
+        }
+
+        public void setNome(string _nome)
+        {
+            this.nome = _nome;
+        }
+
+        public string getSenha()
+        {
+            return this.senha;
+        }
+
+        public void setSenha(string _senha)
+        {
+            this.senha = _senha;
+        }
+
+
+
+        public Usuario(int cpf_, string senha_, string nome_)
+        {
+            cpf = cpf_;
+            senha = senha_;
+            nome = nome_;
         }
 
 
 
     }
 
-    public class Administrador{ //Mudar pras do pedro depois q ele commitar
+    public class Administrador
+    { //Mudar pras do pedro depois q ele commitar
 
-       int cpf;
+        int cpf;
 
-       string nome;
+        string nome;
 
-       string senha;
+        string senha;
 
-       bool adm = true;
+        bool adm = true;
 
-       
-       public int getCpf(){ 
-        return this.cpf;
-       }
 
-       public void setCpf(int _cpf){
-         this.cpf = _cpf;
-       }
-
-        public string getNome(){
-        return this.nome;
-       }
-
-       public void setNome(string _nome){
-         this.nome = _nome;
-       }
-
-        public string getSenha(){
-        return this.senha;
-       }
-
-       public void setSenha(string _senha){
-         this.senha = _senha;
-       }
-    
-
-       public Administrador(int cpf_, string senha_, string nome_)
+        public int getCpf()
         {
-            cpf=cpf_;
-            senha=senha_;
-            nome=nome_;
-           
+            return this.cpf;
         }
 
-        public void criarPartido(){
+        public void setCpf(int _cpf)
+        {
+            this.cpf = _cpf;
+        }
+
+        public string getNome()
+        {
+            return this.nome;
+        }
+
+        public void setNome(string _nome)
+        {
+            this.nome = _nome;
+        }
+
+        public string getSenha()
+        {
+            return this.senha;
+        }
+
+        public void setSenha(string _senha)
+        {
+            this.senha = _senha;
+        }
+
+
+        public Administrador(int cpf_, string senha_, string nome_)
+        {
+            cpf = cpf_;
+            senha = senha_;
+            nome = nome_;
+
+        }
+
+        public void criarPartido()
+        {
 
 
         }
@@ -169,9 +184,9 @@ namespace Classes
 
     }
 
-   public abstract class Eleicao
-{
-     protected int totalDeVotos = 0;
+    public abstract class Eleicao
+    {
+        protected int totalDeVotos = 0;
 
         public int getTotalDeVotos()
         {
@@ -183,9 +198,9 @@ namespace Classes
             totalDeVotos += votos;
         }
 
-}
+    }
 
- public class Candidato //Mudar pras do pedro depois q ele commitar
+    public class Candidato //Mudar pras do pedro depois q ele commitar
     {
         public string Nome { get; set; }
         public int Codigo { get; set; }
@@ -203,43 +218,43 @@ namespace Classes
         }
     }
 
-public class Executivo : Eleicao
-{
-    private Candidato[] candidatos;
-
-    private int turno;
-    public Executivo(int numeroCandidatos)
+    public class Executivo : Eleicao
     {
-        candidatos = new Candidato[numeroCandidatos];
-        turno = 1;
-    }
+        private Candidato[] candidatos;
 
-
-    public void PrimeiroTurno()
-    {
-        bool empate = false;
-           int maxVotos = -1;
-        Candidato vencedor = null;
-
-        turno = 1;
-
-        for (int i = 0; i < candidatos.Length; i++)
+        private int turno;
+        public Executivo(int numeroCandidatos)
         {
-            if (candidatos[i].Votos > maxVotos)
-            {
-                maxVotos = candidatos[i].Votos;
-                vencedor = candidatos[i];
-            }
-            else if (candidatos[i].Votos == maxVotos)
-            {
-                empate = true;
-            }
+            candidatos = new Candidato[numeroCandidatos];
+            turno = 1;
         }
-        //Define os votos do candidato como maxVotos, se tiver outro candidato com a mesma quantidade de votos(maxVotos) define empate como true, vai pro empate e chama o segundo turno
-        
-         if (empate)
+
+
+        public void PrimeiroTurno()
+        {
+            bool empate = false;
+            int maxVotos = -1;
+            Candidato vencedor = null;
+
+            turno = 1;
+
+            for (int i = 0; i < candidatos.Length; i++)
             {
-                
+                if (candidatos[i].Votos > maxVotos)
+                {
+                    maxVotos = candidatos[i].Votos;
+                    vencedor = candidatos[i];
+                }
+                else if (candidatos[i].Votos == maxVotos)
+                {
+                    empate = true;
+                }
+            }
+            //Define os votos do candidato como maxVotos, se tiver outro candidato com a mesma quantidade de votos(maxVotos) define empate como true, vai pro empate e chama o segundo turno
+
+            if (empate)
+            {
+
                 segundoturno();
             }
             else if (vencedor != null)
@@ -247,69 +262,141 @@ public class Executivo : Eleicao
                 Console.WriteLine($"Vencedor do primeiro turno: {vencedor.Nome}");
             }
 
-        
-    }
 
-public void RegistrarCandidato(Candidato candidato, int index) //mudar isso aq
+        }
+
+        public void RegistrarCandidato(Candidato candidato, int index) //mudar isso aq
         {
             candidatos[index] = candidato;
         }
-    
 
-    public void segundoturno()
-    {
-       
-       turno = 2;
 
- var topTwo = candidatos.Where(c => c != null).OrderByDescending(c => c.Votos).Take(2).ToArray();
-   
+        public void segundoturno()
+        {
 
-    Candidato candidato1 = topTwo[0];
-    Candidato candidato2 = topTwo[1];
+            turno = 2;
 
-    turno = 2;
+            var topTwo = candidatos.Where(c => c != null).OrderByDescending(c => c.Votos).Take(2).ToArray();
 
-    Console.WriteLine("A votação deu empate, iremos para o segundo turno!");
 
-    Console.WriteLine($"Coloque os votos referentes ao {candidato1.Nome}!");
-    int votosCandidato1 = int.Parse(Console.ReadLine());
-   
+            Candidato candidato1 = topTwo[0];
+            Candidato candidato2 = topTwo[1];
 
-    Console.WriteLine($"Coloque os votos referentes ao {candidato2.Nome}!");
-    int votosCandidato2 = int.Parse(Console.ReadLine());
-  //Define os candidatos com mais votos pelo "OrderByDescending" e declara eles em variáveis, depois define a quantidade de votos pros 2 e verifica se alguém ganhou a eleição ou se vai pro desempate
+            turno = 2;
 
-      candidato1.Votos = votosCandidato1;
-    candidato2.Votos = votosCandidato2;
+            Console.WriteLine("A votação deu empate, iremos para o segundo turno!");
 
-    if (candidato1.Votos > candidato2.Votos)
-    {
-        Console.WriteLine($"{candidato1.Nome} ganhou a eleição!");
-    }
-    else if (candidato2.Votos > candidato1.Votos)
-    {
-        Console.WriteLine($"{candidato2.Nome} ganhou a eleição!");
-    }
-    else 
-    {
-       Console.WriteLine("Vamos para o desempate!");
-desempate(candidato1, candidato2);
-    }
-    
-    }
- public void desempate(Candidato candidato1, Candidato candidato2)
-    {
+            Console.WriteLine($"Coloque os votos referentes ao {candidato1.Nome}!");
+            int votosCandidato1 = int.Parse(Console.ReadLine());
 
-//No desempate verifica qual é mais velho e define como vencedor
-if (candidato1.Idade > candidato2.Idade)
+
+            Console.WriteLine($"Coloque os votos referentes ao {candidato2.Nome}!");
+            int votosCandidato2 = int.Parse(Console.ReadLine());
+            //Define os candidatos com mais votos pelo "OrderByDescending" e declara eles em variáveis, depois define a quantidade de votos pros 2 e verifica se alguém ganhou a eleição ou se vai pro desempate
+
+            candidato1.Votos = votosCandidato1;
+            candidato2.Votos = votosCandidato2;
+
+            if (candidato1.Votos > candidato2.Votos)
             {
-              Console.WriteLine($"{candidato1.Nome} ganhou a eleição!");
-               
-            } else {
+                Console.WriteLine($"{candidato1.Nome} ganhou a eleição!");
+            }
+            else if (candidato2.Votos > candidato1.Votos)
+            {
+                Console.WriteLine($"{candidato2.Nome} ganhou a eleição!");
+            }
+            else
+            {
+                Console.WriteLine("Vamos para o desempate!");
+                desempate(candidato1, candidato2);
+            }
 
-Console.WriteLine($"{candidato2.Nome} ganhou a eleição!");
+        }
+        public void desempate(Candidato candidato1, Candidato candidato2)
+        {
+
+            //No desempate verifica qual é mais velho e define como vencedor
+            if (candidato1.Idade > candidato2.Idade)
+            {
+                Console.WriteLine($"{candidato1.Nome} ganhou a eleição!");
+
+            }
+            else
+            {
+
+                Console.WriteLine($"{candidato2.Nome} ganhou a eleição!");
+
+            }
+
+        }
+
+        public class Legislativo : Eleicao
+        {
+            private List<PartidoLegislativo> partidos;
+            private int cadeirasDisponiveis;
+
+            public Legislativo()
+            {
+                partidos = new List<PartidoLegislativo>();
+            }
+
+            public void setCadeirasDisponiveis(int _cadeirasDisponiveis)
+            {
+                this.cadeirasDisponiveis = _cadeirasDisponiveis;
+            }
+            public int getCadeirasDisponiveis()
+            {
+                return this.cadeirasDisponiveis;
+            }
+
+            public void RegistrarPartido(PartidoLegislativo partido)
+            {
+                partidos.Add(partido);
+            }
+
+            public void calcularMedia(PartidoLegislativo partido)
+            {
+                return partido.getQuocienteEleitoral / (partido.getCadeiras + 1);
+            }
+
+            public void atribuirCadeirasSobrando()
+            {
+                for (int j = 0; j < cadeirasDisponiveis; j++)
+                {
+                    int sobra = 0;
+                    int posPartido = -1;
+                    for (int i = 0; i < partidos.Count; i++)
+                    {
+                        int atual = calcularMedia(partidos[i]);
+                        if (atual > sobra)
+                        {
+                            sobra = atual;
+                            posPartido = i;
+                        }
+                    }
+                    if (posPartido != -1)
+                    {
+                        partidos[posPartido].setVotosRecebidos(partidos[posPartido].getVotosRecebidos() + 1);
+                    }
+                }
+            }
+
+            public void calcularQuocientes()
+            {
+                for (int i = 0; i < partidos.Count; i++)
+                {
+                    partidos[i].calcularQuociente(getTotalDeVotos(), cadeirasDisponiveis);
+                }
+            }
+
+            public void calcularCadeiras()
+            {
+                for (int i = 0; i < partidos.Count; i++)
+                {
+                    partidos[i].calcularCadeiras(partidos[i].getVotosRecebidos(), partidos[i].getQuocienteEleitoral());
+                }
+            }
+        }
 
     }
-    
 }
-}}
